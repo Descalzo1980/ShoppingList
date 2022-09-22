d61778c18d645f5ba5d27fb6f0583fd1316737c0
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-09-22 22:28:26 +0300

    add domain classes

---
+++ app/src/main/java/com/example/shoppinglist/domain/GetShopItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.shoppinglist.domain
+
+class GetShopItemUseCase {
+
+    fun getShopItem(shopItemId: Int):ShopItem{ // принимает один id возвращает shop item
+        TODO()
+    }
+}
